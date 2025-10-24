216713fafd8c86d367d2b3b7b9c2a1fd2732bfb3
Hemanth-4301 <hemanthachar526@gmail.com>
2025-10-24 16:40:49 +0530

    modified the client url

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin:["http://localhost:5173","https://veriloc.vercel.app"],
+  origin:["http://localhost:5173","https://veriloc-six.vercel.app"],
   credentials:true
 }));
 app.use(express.json());
