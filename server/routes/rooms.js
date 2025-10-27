a6767d29ff3f93aac1306330803137527c713855
Hemanth-4301 <hemanthachar526@gmail.com>
2025-10-27 21:40:00 +0530

    added message

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -405,7 +405,7 @@ router.post(
       );
 
       res.json({
-        message: "Room status updated successfully ganu bro..",
+        message: "gorgeous ganu bro..",
         room: {
           roomNumber: room.roomNumber,
           status: room.status,
